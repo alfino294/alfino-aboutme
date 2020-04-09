c90839965fa336315fd2588ac6678958ecb9b69f
alfino putra laksana <63408974+alfino294@users.noreply.github.com>
2020-04-09 21:41:41 +0700

    Create aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,10 @@
+<?php
+echo 'Nama : Alfino putra laksana <br>' ;
+echo 'Kelas : XI RPL 1 <br>';
+echo 'Asal Sekolah : SMK Taruna Bhakti Depok <br>';
+echo 'No handphone : 0895704268728 <br>';
+
+
+
+
+?>
