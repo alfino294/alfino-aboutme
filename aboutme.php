1e0ed5584fc12ba504f95eb714e3676dd967b450
alfino putra laksana <63408974+alfino294@users.noreply.github.com>
2020-04-09 21:54:18 +0700

    menambahkanaboutme.php

--- aboutme.php
+++ aboutme.php
@@ -3,6 +3,7 @@ echo 'Nama : Alfino putra laksana <br>' ;
 echo 'Kelas : XI RPL 1 <br>';
 echo 'Asal Sekolah : SMK Taruna Bhakti Depok <br>';
 echo 'No handphone : 0895704268728 <br>';
+echo 'Alamat : jln.mandai 2 no.17 <br>';
 
 
 
